aae9e33da2bdce8a0b56b092bad6786bcdfbc92f
lstroly <lstroly@gmail.com>
2018-03-15 20:54:09 -0400

    Login page GET & POST routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,28 @@
 // Requiring our models for syncing
-var db = require("../models");
+var User = require("../models/user.js"); //FILE NAME MAY DIFFER
 
 // Routes
 // =============================================================
 module.exports = function(app) {
+
+  //Routes for login page. GET existing account & POST new account
+
+  app.get("api/users", function(req, res) {
+    User.findAll({}).then(function(result) {
+      return res.json(result);
+    });
+  });
+
+  app.post("api/new", function(req, res) {
+    User.create({
+      email_id: req.body.email_id,
+      pass_word: req.body.pass_word,
+      last_name: req.body.last_name,
+      first_name: req.body.first_name
+    })
+  });
+
+// =============================================================
 
   app.get(/*ROUTE GOES HERE*/, function(req, res) {
     /*SEQUELIZE CODE GOES HERE*/
