dd12b291cdbc8fc576579be0526728a6597550ee
Matt Gohr <gohr89@gmail.com>
2018-03-18 15:36:03 -0400

    adding views file

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -6,7 +6,64 @@ module.exports = function(app) {
 
   // index route loads view.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+
+    // res.sendFile(path.join(__dirname, "../public/login.html"));
+
+    res.render("index");
+
+
+  });
+
+  // index route loads view.html
+  app.get("/acctDetail.js", function(req, res) {
+
+    // res.sendFile(path.join(__dirname, "../public/login.html"));
+
+    res.sendFile(path.join(__dirname, "../public/js/acctDetail.js"));
+
+  });
+
+  app.get("/api/:param?", function(req, res) {
+    res.render("index")
+    console.log(param);
+    console.log(res);
+    console.log(req);
+  });
+  // index route loads view.html
+  app.get("/acctList.js", function(req, res) {
+
+    // res.sendFile(path.join(__dirname, "../public/login.html"));
+
+    res.render("index");
+
+
+  });
+  // index route loads view.html
+  app.get("/add.js", function(req, res) {
+
+    // res.sendFile(path.join(__dirname, "../public/login.html"));
+
+    res.render("index");
+
+
+  });
+  // index route loads view.html
+  app.get("/login.js", function(req, res) {
+
+    // res.sendFile(path.join(__dirname, "../public/login.html"));
+
+    res.render("index");
+
+
+  });
+  // index route loads view.html
+  app.get("/view.js", function(req, res) {
+
+    // res.sendFile(path.join(__dirname, "../public/login.html"));
+
+    res.render("index");
+
+
   });
 
   // add route loads the add.html page,
--- app/server.js
+++ app/server.js
@@ -1,5 +1,3 @@
-// Dependencies
-// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
 
@@ -7,8 +5,23 @@ var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Requiring our models for syncing
-var db = require("./app/models/broker.js");
+var db = require("./models/broker.js");
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,13 +32,19 @@ app.use(express.static("public"));
 
 // Dependencies
 // =============================================================
-require("./app/routes/api-routes.js")(app);
-require("./app/routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true}).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+
 });
+// db.sequelize.sync({ force: true}).then(function() {
+//   app.listen(PORT, function() {
+//     console.log("App listening on PORT " + PORT);
+//   });
+// });
