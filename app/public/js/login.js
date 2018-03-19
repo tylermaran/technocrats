e4a30c4b82bd427fb9c6a6c39c6a3920d9edaa4b
lstroly <lstroly@gmail.com>
2018-03-19 09:10:55 -0400

    routing updates

--- app/public/js/login.js
+++ app/public/js/login.js
@@ -1,5 +1,64 @@
 $( document ).ready(function() {
-    console.log("hello!")
+  console.log("hello!");
+
+  // when user clicks "Login"
+  //======================================================
+  $("#run-search").on("click", function(event) {
+    event.preventDefault();
+
+    var searchedUser = email_id: $("#email_id").val().trim();
+    searchedUser = searchedUser.replace(/\s+/g, "").toLowerCase();
+
+    // send an AJAX GET-request
+    $.get("api/" + searchedUser, function(data) {
+
+      if (!data) {
+        alert("Sorry, your email or password doesn't match what we have on file.");
+      } else if (/*Check if password does NOT match*/){
+        alert("Sorry, your email or password doesn't match what we have on file.");
+      } else {
+
+        //Open user's account page
+
+      }
+
+
+    })
+
+    $("#email_id").val("");
+    $("#pass_word").val("");
+
+  });
+
+
+  // when user clicks "Enroll"
+  //======================================================
+  $("#enrollBtn").on("click", function(event) {
+    event.preventDefault();
+
+    // make a newUser obj
+    var newUser = {
+      email_id: $("#email_id").val().trim(),
+      pass_word: $("#pass_word").val().trim()
+    }
+
+    // send an AJAX POST-request with jQuery
+    $.post("api/new")
+    .then(function(data) {
+
+      //Open new user's account page
+
+    });
+
+    $("#email_id").val("");
+    $("#pass_word").val("");
+
+  });
+
+
+
+//Video Background
+//======================================================
     scaleVideoContainer();
 
     initBannerVideoSize('.video-container .poster img');
