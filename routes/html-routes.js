df6a63a584d9a8afd068a02552450a97999f00fd
lstroly <lstroly@gmail.com>
2018-03-22 11:51:17 -0400

    about update

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -17,7 +17,7 @@ module.exports = function(app) {
   app.get("/summary", function(req, res) {
     var items = {
       stylesheet: `<link rel='stylesheet' type='text/css' href='/style.css'>`,
-      title: `Summary`, 
+      title: `Summary`,
       sumActive: true
     }
     res.render("mySummary", items);
@@ -36,7 +36,7 @@ module.exports = function(app) {
   //route to about.handlebars
   app.get("/about", function(req, res) {
     var items = {
-      stylesheet: `<link rel='stylesheet' type='text/css' href='/style.css'>`,
+      stylesheet: `<link rel='stylesheet' type='text/css' href='/about.css'>`,
       title: `About`,
       abtActive: true
     }
