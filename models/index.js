ee55baf741cde1ea9d8294067fb166e12572f1dc
lstroly <lstroly@gmail.com>
2018-03-22 19:22:33 -0400

    modifies config variables to use Heroku env variables

--- models/index.js
+++ models/index.js
@@ -8,8 +8,13 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+if (process.env.NODE_ENV && process.env.NODE_ENV === "production") {
+  var sequelize = new Sequelize({
+    host: process.env.DB_HOST,
+    database: process.env.DB,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD
+  });
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
