9bab9baad8652a0e3878d42c6519df35d33f5838
Tyler Maran <tyler.maran@gmail.com>
2018-03-22 20:39:00 -0400

    adding cookie to login

--- public/js/acctDetail.js
+++ public/js/acctDetail.js
@@ -48,55 +48,97 @@ const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
 var queryURLBase = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=DJI&apikey=QXOWRDKW82K1ZDBF";
 
+$( document ).ready(function() {
+  console.log("acctDetail.js Loaded!");
+
+// ************************************************************************
+    $.get("/api/transaction/" + searchedEmail, function(data) {
+
+      if (!data) {
+        // No matching email in the database
+        alert("Sorry, no account info found");
+        return;
+      } 
+      // if (data.pass_word===searchedPass) {
+      //   console.log("Correct password");
+      //   window.location = "/summary";
+      // }
+      // else {
+      //   alert("Sorry, that password doesn't match " + data.email_id);
+      //   $("#pass_word").val("");
+      // }
+
+    console.log(data);
+     
+    for (var i = 0; i < acctDetailDB.length; i++) {
+        $('#targetbody').append($('<tr>')
+            .append($('<td>').append(acctDetailDB[i].tran_id))
+            .append($('<td>').append(acctDetailDB[i].acct_id))
+            .append($('<td>').append(acctDetailDB[i].tran_datetime))
+            .append($('<td>').append(acctDetailDB[i].tran_symbol))
+            .append($('<td>').append(acctDetailDB[i].tran_desc))
+            .append($('<td>').append(acctDetailDB[i].symbol_quote))
+            .append($('<td>').append(acctDetailDB[i].symbol_delta))
+            .append($('<td>').append(acctDetailDB[i].symbol_quantity))
+            .append($('<td>').append(acctDetailDB[i].symbol_cost_share))
+            .append($('<td>').append(acctDetailDB[i].symbol_cost_basis))
+            .append($('<td>').append(acctDetailDB[i].symbol_gain_loss))
+            .append($('<td>').append(acctDetailDB[i].symbol_market_value))
+        ) 
+    } 
+
+    });
+
+  });
+
+// ************************************************************************
 
 
-function runQuery(queryURLBase) {
-
-    $.ajax({
-        //url: proxyurl + queryURLBase,
-        url: queryURLBase,
-        method: "GET"
-    })
-
-    .done(function(response) {
-        //form input variables 
-        var acctdata = response;
 
 
-        // Logging the URL so we have access to it for troubleshooting
-        console.log("------------------------------------");
-        console.log("Full Data:");
-        console.log(acctdata);
-        console.log("------------------------------------");
+// function runQuery(queryURLBase) {
 
-        // parse JSON and Display in TABLE
-        for (var i = 0; i < acctDetailDB.length; i++) {
+//     $.ajax({
+//         //url: proxyurl + queryURLBase,
+//         url: queryURLBase,
+//         method: "GET"
+//     })
 
-            $('#targetbody').append($('<tr>')
-                    .append($('<td>').append(acctDetailDB[i].tran_id))
-                    .append($('<td>').append(acctDetailDB[i].acct_id))
-                    .append($('<td>').append(acctDetailDB[i].tran_datetime))
-                    .append($('<td>').append(acctDetailDB[i].tran_symbol))
-                    .append($('<td>').append(acctDetailDB[i].tran_desc))
-                    .append($('<td>').append(acctDetailDB[i].symbol_quote))
-                    .append($('<td>').append(acctDetailDB[i].symbol_delta))
-                    .append($('<td>').append(acctDetailDB[i].symbol_quantity))
-                    .append($('<td>').append(acctDetailDB[i].symbol_cost_share))
-                    .append($('<td>').append(acctDetailDB[i].symbol_cost_basis))
-                    .append($('<td>').append(acctDetailDB[i].symbol_gain_loss))
-                    .append($('<td>').append(acctDetailDB[i].symbol_market_value))
-                ) // <tr>
-
-            // } // IF Station === 
-
-        } //FOR LOOP
+//     .done(function(response) {
+//         //form input variables 
+//         var acctdata = response;
 
 
-    }); // ; for .done & .fail
+//         // Logging the URL so we have access to it for troubleshooting
+//         console.log("------------------------------------");
+//         console.log("Full Data:");
+//         console.log(acctdata);
+//         console.log("------------------------------------");
 
-}
+//         // parse JSON and Display in TABLE
+//         for (var i = 0; i < acctDetailDB.length; i++) {
 
-// METHODS
-// ==========================================================
+//             $('#targetbody').append($('<tr>')
+//                     .append($('<td>').append(acctDetailDB[i].tran_id))
+//                     .append($('<td>').append(acctDetailDB[i].acct_id))
+//                     .append($('<td>').append(acctDetailDB[i].tran_datetime))
+//                     .append($('<td>').append(acctDetailDB[i].tran_symbol))
+//                     .append($('<td>').append(acctDetailDB[i].tran_desc))
+//                     .append($('<td>').append(acctDetailDB[i].symbol_quote))
+//                     .append($('<td>').append(acctDetailDB[i].symbol_delta))
+//                     .append($('<td>').append(acctDetailDB[i].symbol_quantity))
+//                     .append($('<td>').append(acctDetailDB[i].symbol_cost_share))
+//                     .append($('<td>').append(acctDetailDB[i].symbol_cost_basis))
+//                     .append($('<td>').append(acctDetailDB[i].symbol_gain_loss))
+//                     .append($('<td>').append(acctDetailDB[i].symbol_market_value))
+//                 ) 
 
-runQuery(queryURLBase);
+//         } 
+
+//     }); 
+
+// }
+
+// }
+
+// runQuery(queryURLBase);
