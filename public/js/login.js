d58febc7e775dda54270f6cc1be8c04569cf9e20
Matt Gohr <gohr89@gmail.com>
2018-03-23 18:33:27 -0400

    Adding login modal

--- public/js/login.js
+++ public/js/login.js
@@ -10,7 +10,7 @@ $( document ).ready(function() {
     searchedUser = searchedUser.replace(/\s+/g, "").toLowerCase();
 
     if (searchedUser==="" || searchedPass==="") {
-      alert("You must enter all forms");
+
       return;
     }
 
@@ -21,55 +21,150 @@ $( document ).ready(function() {
 
       if (!data) {
         // No matching email in the database
-        alert("Sorry, your email doesn't match what we have on file.");
         $("#email_id").val("");
         $("#pass_word").val("");
         return;
-      } 
+      }
       if (data.pass_word===searchedPass) {
         console.log("Correct password");
         window.location = "/summary";
       }
       else {
-        alert("Sorry, that password doesn't match " + data.email_id);
         $("#pass_word").val("");
       }
 
     });
 
   });
+  $(".modal-trigger").on("click", function() {
+    console.log('clicked trigger');
+    // alert("hey")
 
+    $('#modal1').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      inDuration: 300, // Transition in duration
+      outDuration: 200, // Transition out duration
+      startingTop: '4%', // Starting top style attribute
+      endingTop: '10%', // Ending top style attribute
+      // ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+      //   alert("Ready");
+      //   console.log(modal, trigger);
+      // },
+      // complete: function() {
+      //   alert('Closed');
+      // } // Callback for Modal close
+    });
+  });
 
   // when user clicks "Enroll"
   //======================================================
   $("#enrollBtn").on("click", function(event) {
     event.preventDefault();
 
-    // make a newUser obj
-    var newUser = {
-      email_id: $("#email_id").val().trim(),
-      pass_word: $("#pass_word").val().trim(),
-      last_name: "LastName",
-      first_name: "FirstName"
-    }
-    
-   
-    $.post("/api/new", newUser).then(function(data) {
-
-      alert("New user added!");
-      $("#enrollBtn").attr("href", "mySummary.html");
-
+    $('#modal1').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      inDuration: 300, // Transition in duration
+      outDuration: 200, // Transition out duration
+      startingTop: '4%', // Starting top style attribute
+      endingTop: '10%', // Ending top style attribute
+      complete: function() {
+        // Callback for Modal close
+        //put submit function in here
+      }
     });
-    
-
-    $("#email_id").val("");
-    $("#pass_word").val("");
+    //waiting to comeplete this
+    // var newUser = {
+    //   email_id: $("#email_id").val().trim(),
+    //   pass_word: $("#pass_word").val().trim(),
+    //   last_name: "LastName",
+    //   first_name: "FirstName"
+    // }
+    //
+    //
+    // $.post("/api/new", newUser).then(function(data) {
+    //
+    //   $("#enrollBtn").attr("href", "mySummary.html");
+    //
+    // });
+    //
+    //
+    // $("#email_id").val("");
+    // $("#pass_word").val("");
 
   });
 
 });
 
+  //needs work
+  $("#formValidate").on('submit', function(e) {
+    var isvalid = $("#signup_submit").valid();
+    if (isvalid) {
+      e.preventDefault();
+      // alert(getvalues("myform"));
+      console.log('no submit');
+    }
+  });
+  $(".formValidate").validate({
+    rules: {
+      fname: {
+        required: true,
+        minlength: 2
+      },
+      lname: {
+        required: true,
+        minlength: 2
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      password: {
+        required: true,
+        minlength: 5
+      },
+      confirm_password: {
+        required: true,
+        minlength: 5,
+        equalTo: "#password"
+      }
+    },
 
+    //For custom messages
+    messages: {
+      fname: {
+        required: "Please enter your first name.",
+        minlength: "You sure you're named with one letter?"
+      },
+      lname: {
+        required: "Please enter your last name.",
+        minlength: "You sure you're named with one letter?"
+      },
+      email: {
+        required: "Please enter your email address.",
+        email: "Please enter a valid email address."
+      },
+      pass: {
+        required: "Please enter a password.",
+        minlength: "Password must be atleast 5 characters."
+      },
+      confirm_pass: {
+        required: "Please confirm your password.",
+        minlength: "Password must be atleast 5 characters.",
+        equalTo: "Password does not match."
+      }
+    },
+    errorElement: 'div',
+    errorPlacement: function(error, element) {
+      var placement = $(element).data('error');
+      if (placement) {
+        $(placement).append(error)
+      } else {
+        error.insertAfter(element);
+      }
+    }
+  });
 
 //Video Background
 //======================================================
