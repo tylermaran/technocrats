a89764dfe294ba71ecdb05c016de2ec05fd88152
Matt Gohr <gohr89@gmail.com>
2018-03-24 07:18:29 -0400

    modal handelbars updated

--- public/js/login.js
+++ public/js/login.js
@@ -64,109 +64,99 @@ $(document).ready(function() {
         event.preventDefault();
 
         $('#modal1').modal({
-            dismissible: true, // Modal can be dismissed by clicking outside of the modal
-            opacity: .5, // Opacity of modal background
-            inDuration: 300, // Transition in duration
-            outDuration: 200, // Transition out duration
-            startingTop: '4%', // Starting top style attribute
-            endingTop: '10%', // Ending top style attribute
-            complete: function() {
-                // Callback for Modal close
-                //put submit function in here
-            }
+          dismissible: true, // Modal can be dismissed by clicking outside of the modal
+          opacity: .5, // Opacity of modal background
+          inDuration: 500, // Transition in duration
+          outDuration: 200, // Transition out duration
+          startingTop: '4%', // Starting top style attribute
+          endingTop: '10%', // Ending top style attribute
+          complete: function() {
+            console.log('modal sucess');
+          }
         });
-        //waiting to comeplete this
-        // var newUser = {
-        //   email_id: $("#email_id").val().trim(),
-        //   pass_word: $("#pass_word").val().trim(),
-        //   last_name: "LastName",
-        //   first_name: "FirstName"
-        // }
-        //
-        //
-        // $.post("/api/new", newUser).then(function(data) {
-        //
-        //   $("#enrollBtn").attr("href", "mySummary.html");
-        //
-        // });
-        //
-        //
-        // $("#email_id").val("");
-        // $("#pass_word").val("");
 
     });
 
 });
 
-//needs work
-$("#formValidate").on('submit', function(e) {
-    var isvalid = $("#signup_submit").valid();
-    if (isvalid) {
-        e.preventDefault();
-        // alert(getvalues("myform"));
-        console.log('no submit');
-    }
-});
+
 $(".formValidate").validate({
-    rules: {
-        fname: {
-            required: true,
-            minlength: 2
-        },
-        lname: {
-            required: true,
-            minlength: 2
-        },
-        email: {
-            required: true,
-            email: true
-        },
-        password: {
-            required: true,
-            minlength: 5
-        },
-        confirm_password: {
-            required: true,
-            minlength: 5,
-            equalTo: "#password"
-        }
+  rules: {
+    fname: {
+      required: true,
+      minlength: 2
     },
-
-    //For custom messages
-    messages: {
-        fname: {
-            required: "Please enter your first name.",
-            minlength: "You sure you're named with one letter?"
-        },
-        lname: {
-            required: "Please enter your last name.",
-            minlength: "You sure you're named with one letter?"
-        },
-        email: {
-            required: "Please enter your email address.",
-            email: "Please enter a valid email address."
-        },
-        pass: {
-            required: "Please enter a password.",
-            minlength: "Password must be atleast 5 characters."
-        },
-        confirm_pass: {
-            required: "Please confirm your password.",
-            minlength: "Password must be atleast 5 characters.",
-            equalTo: "Password does not match."
-        }
+    lname: {
+      required: true,
+      minlength: 2
     },
-    errorElement: 'div',
-    errorPlacement: function(error, element) {
-        var placement = $(element).data('error');
-        if (placement) {
-            $(placement).append(error)
-        } else {
-            error.insertAfter(element);
-        }
+    email: {
+      required: true,
+      email: true
+    },
+    password: {
+      required: true,
+      minlength: 5
+    },
+    confirm_password: {
+      required: true,
+      minlength: 5,
+      equalTo: "#password"
     }
-});
+  },
 
+  //For custom messages
+  messages: {
+    fname: {
+      required: "Please enter your first name.",
+      minlength: "You sure you're named with one letter?"
+    },
+    lname: {
+      required: "Please enter your last name.",
+      minlength: "You sure you're named with one letter?"
+    },
+    email: {
+      required: "Please enter your email address.",
+      email: "Please enter a valid email address."
+    },
+    pass: {
+      required: "Please enter a password.",
+      minlength: "Password must be atleast 5 characters."
+    },
+    confirm_pass: {
+      required: "Please confirm your password.",
+      minlength: "Password must be atleast 5 characters.",
+      equalTo: "Password does not match."
+    }
+  },
+  errorElement: 'div',
+  errorPlacement: function(error, element) {
+    var placement = $(element).data('error');
+    if (placement) {
+      $(placement).append(error)
+    } else {
+      error.insertAfter(element);
+    }
+  },
+  submitHandler: function() {
+    // alert("Submitted!");
+    var newUser = {
+      email_id: $("#email").val().trim(),
+      pass_word: $("#password").val().trim(),
+      last_name: $("#fname").val().trim(),
+      first_name: $("#lname").val().trim()
+    }
+
+
+    $.post("/api/new", newUser).then(function(data) {
+
+      $("#enrollBtn").attr("href", "mySummary.html");
+      console.log('posted');
+
+    });
+  }
+
+});
 //Video Background
 //======================================================
 // scaleVideoContainer();
